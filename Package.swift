1f049e4f0ae5ccfcb4766dd2a90682f1375e8b02
Adam Dangoor <adamdangoor@gmail.com>
2026-07-02 11:51:37 +0100

    Enable DocC plugin for documentation workflow

--- Package.swift
+++ Package.swift
@@ -1,4 +1,5 @@
 // swift-tools-version: 6.2
+import Foundation
 import PackageDescription
 
 let package = Package(
@@ -32,3 +33,11 @@ let package = Package(
         )
     ]
 )
+
+// Pull in swift-docc-plugin only for the GitHub Pages documentation workflow,
+// so consumers do not resolve it during normal package use.
+if ProcessInfo.processInfo.environment["PRINTABLEVIEW_BUILD_DOCS"] != nil {
+    package.dependencies.append(
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
+    )
+}
